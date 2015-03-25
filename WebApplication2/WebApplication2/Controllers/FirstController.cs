abc9ef6c3f024c05413409acf38531f5b12c2691
AlbinaV <alv_t@mail.ru>
2015-03-26 00:49:14 +0500

    Lab7

---
+++ WebApplication2/WebApplication2/Controllers/FirstController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+   public class FirstController : Controller
+    {
+        //
+        // GET: /first/
+        public ActionResult View1()
+        {
+
+           ViewBag.Message = "Привет! " ; 
+           ViewBag.Message1 = "Сейчас "  + DateTime.Now; 
+            return View();
+        }
+    }
+}
+
---
+++ WebApplication2/WebApplication2/Controllers/SecondController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+  public class SecondController : Controller
+    {
+        //
+        // GET: /second/
+        public ActionResult View1()
+        {
+            ViewBag.Message = "Привет!";
+            return View();
+        }
+    }
+}
